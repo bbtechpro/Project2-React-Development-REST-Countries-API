dde87b4999a6c9cabfc7c7d5f9d510bf64dd5e26
Valerie Bolden <valerie.bolden@gmail.com>
2026-05-05 13:58:15 -0400

    added label element with nested input and span to create theme toggle switch

--- src/components/filter/Header.jsx
+++ src/components/filter/Header.jsx
@@ -2,6 +2,12 @@ function Header() {
     return (
         <nav>
             <span>Where in the World?</span>
+            <label className="switch">
+                <input type="checkbox" id="toggle"></input>
+                <span className="slider round">
+                {/* ICON GOES HERE */}
+                </span>
+            </label>
         </nav>
     )
 }
