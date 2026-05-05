6636be8b4eed327cbcc76f685dd7c180ac456479
Valerie Bolden <valerie.bolden@gmail.com>
2026-05-05 14:51:23 -0400

    added useState for darkMode

--- src/components/filter/Header.jsx
+++ src/components/filter/Header.jsx
@@ -1,13 +1,17 @@
-import { FiMoon, FaSun } from "react-icons/fi";
+import { IoMoon } from "react-icons/io5";
+import { FaSun } from "react-icons/fa";
+import { useState } from "react";
 
 function Header() {
+    const [darkMode, setDarkMode] = useState(false);
+
     return (
         <nav>
             <span>Where in the World?</span>
             <label className="switch">
                 <input type="checkbox" id="toggle"></input>
                 <span className="slider round">
-                <FiMoon />
+                    <IoMoon className="moon-icon" />
                 </span>
             </label>
         </nav>
