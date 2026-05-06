0bf9f4e8fd8b6506cd84a49a7dc4482f610770cf
Valerie Bolden <valerie.bolden@gmail.com>
2026-05-06 08:40:29 -0400

    created an array for filter options

--- src/components/filter/RegionFilter.jsx
+++ src/components/filter/RegionFilter.jsx
@@ -3,6 +3,15 @@ import { useState } from "react";
 
 function RegionFilter() {
     const [value, setValue] = useState([]);
+
+    const options = [
+        {label: "All", value: ""},
+        {label: "Africa", value: "africa"},
+        {label: "Americas", value: "americas"},
+        {label: "Asia", value: "asia"},
+        {label: "Europe", value: "europe"},
+        {label: "Oceania", value: "oceania"},
+    ]
 }
 
 export default RegionFilter;
