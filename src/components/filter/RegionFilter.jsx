836dad16721a18c63c19a2439c8cdcb8e55093fb
Valerie Bolden <valerie.bolden@gmail.com>
2026-05-06 08:49:42 -0400

    added curly braces to Select import

--- src/components/filter/RegionFilter.jsx
+++ src/components/filter/RegionFilter.jsx
@@ -1,23 +1,23 @@
-import Select from "react-dropdown-select";
+import { Select } from "react-dropdown-select";
 import { useState } from "react";
 
 function RegionFilter() {
     const [value, setValue] = useState([]);
 
     const options = [
-        {label: "All", value: ""},
-        {label: "Africa", value: "africa"},
-        {label: "Americas", value: "americas"},
-        {label: "Asia", value: "asia"},
-        {label: "Europe", value: "europe"},
-        {label: "Oceania", value: "oceania"},
+        { label: "All", value: "" },
+        { label: "Africa", value: "africa" },
+        { label: "Americas", value: "americas" },
+        { label: "Asia", value: "asia" },
+        { label: "Europe", value: "europe" },
+        { label: "Oceania", value: "oceania" },
     ];
 
     return (
-        <Select 
-        options={options}
-        values={value}
-        onChange={(values) => setValue(values)}
+        <Select
+            options={options}
+            values={value}
+            onChange={(values) => setValue(values)}
         />
     )
 }
