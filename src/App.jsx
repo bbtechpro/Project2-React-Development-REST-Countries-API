62369b64ca379c685d14269c3046a72930dd4897
Valerie Bolden <valerie.bolden@gmail.com>
2026-05-06 10:23:40 -0400

    removed SearchInput and RegionFilter components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
 import './App.css'
 import Header from './components/filter/Header'
-import SearchInput from './components/filter/SearchInput'
-import RegionFilter from './components/filter/RegionFilter'
 import HomePage from './components/pages/Home'
 
 function App() {
@@ -9,10 +7,6 @@ function App() {
   return (
     <>
       <Header />
-      <div className="filter-search">
-        <SearchInput />
-        <RegionFilter />
-      </div>
       <HomePage />
     </>
   )
