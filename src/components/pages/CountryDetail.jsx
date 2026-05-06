fe92fd3beba62cd451e5a7c99202dd5a96321ba8
Valerie Bolden <valerie.bolden@gmail.com>
2026-05-06 13:46:35 -0400

    added more elements to UI

--- src/components/pages/CountryDetail.jsx
+++ src/components/pages/CountryDetail.jsx
@@ -10,6 +10,24 @@ function CountryDetail() {
                     Back
                     <IoMdHome />
                 </button>
+                <div className="flexbox">
+                    <div className="flag-container">
+                        <img src="" alt="" id="flag-detail" />
+                    </div>
+                    <div className="details-right">
+                        <h2>Peru</h2>
+                        <div className="info">
+                            <p><strong>Native Name: </strong></p>
+                            <p><strong>Population: </strong></p>
+                            <p><strong>Region: </strong></p>
+                            <p><strong>Sub Region: </strong></p>
+                            <p><strong>Capital: </strong></p>
+                            <p><strong>Top Level Domain: </strong></p>
+                            <p><strong>Languages: </strong></p>
+                            <p><strong>Currencies: </strong></p>
+                        </div>
+                    </div>
+                </div>
             </div>
         </>
     )
