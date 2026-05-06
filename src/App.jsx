9be09ac2bfd56a4689111515f1a89d0017f8b85e
uries242 <teykoa@gmail.com>
2026-05-05 20:53:44 -0400

    Refactored Context management, added CountryContext and integrate useCountries hook for state handling

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,29 @@
-import { CountryProvider } from "./backend/context/CountryContext"
-import './App.css'
+import { CountryProvider } from "./backend/context/CountryContext.jsx";
+import { fetchAllCountries } from "./backend/services/countriesAPI";
+import "./App.css";
+
+// Temporary test — delete @ production!
+fetchAllCountries()
+  .then((data) => {
+    console.log("✅ API working! First country:", data[0]);
+  })
+  .catch((err) => {
+    console.error("❌ API failed:", err.message);
+  });
+
 
 function App() {
+  return(
+    <CountryProvider>
 
-  return (
-    <>
-    </>
-
+    </CountryProvider>
   )
 }
 
-export default App
-
-
-
-
+export default App;
 
 // Place all Components between the CountryProvider wrapper
 // eg
-
 // return (
 //   <CountryProvider>
 //     <Navbar />
