6d8618fce73a6fcea630dc9e68d1ed0f2f204869
bbtechpro <134446902+bbtechpro@users.noreply.github.com>
2026-05-06 11:18:50 -0400

    Edited changes after merge

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import './App.css'
 import Header from './components/filter/Header'
 import HomePage from './components/pages/Home'
+// import { CountryProvider } from "./backend/context/CountryContext.jsx";
+// import { fetchAllCountries } from "./backend/services/countriesAPI";
 
 function App() {
 
@@ -9,28 +11,8 @@ function App() {
       <Header />
       <HomePage />
     </>
-import { CountryProvider } from "./backend/context/CountryContext.jsx";
-import { fetchAllCountries } from "./backend/services/countriesAPI";
-import "./App.css";
-
-// Temporary test — delete @ production!
-fetchAllCountries()
-  .then((data) => {
-    console.log("✅ API working! First country:", data[0]);
-  })
-  .catch((err) => {
-    console.error("❌ API failed:", err.message);
-  });
-
-
-function App() {
-  return(
-    <CountryProvider>
-
-    </CountryProvider>
   )
 }
-
 export default App;
 
 // Place all Components between the CountryProvider wrapper
@@ -41,3 +23,20 @@ export default App;
 //     <CountryList />
 //   </CountryProvider>
 // )
+
+// Temporary test — delete @ production!
+// fetchAllCountries()
+//   .then((data) => {
+//     console.log("✅ API working! First country:", data[0]);
+//   })
+//   .catch((err) => {
+//     console.error("❌ API failed:", err.message);
+//   });
+
+// function App() {
+//   return(
+//     <CountryProvider>
+
+//     </CountryProvider>
+//   )
+// }
