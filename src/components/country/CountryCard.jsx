f93b4de6f96d4cd130e92e04d4568998d7f539fa
Valerie Bolden <valerie.bolden@gmail.com>
2026-05-06 10:48:08 -0400

    removed extra div from UI

--- src/components/country/CountryCard.jsx
+++ src/components/country/CountryCard.jsx
@@ -1,14 +1,12 @@
 function CountryCard() {
     return (
-        <div className="wrapper">
-            <div className="card">
-                <img className="flag" src="https://flagcdn.com/pe.svg" alt="" />
-                <div className="card-body">
-                    <h5>Peru</h5>
-                    <p>Population: </p>
-                    <p>Region: </p>
-                    <p>Capital: </p>
-                </div>
+        <div className="card">
+            <img className="flag" src="https://flagcdn.com/pe.svg" alt="" />
+            <div className="card-body">
+                <h3>Peru</h3>
+                <p>Population: </p>
+                <p>Region: </p>
+                <p>Capital: </p>
             </div>
         </div>
     );
