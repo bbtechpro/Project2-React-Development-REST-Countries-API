91c03f3b11a87e549dafdfe4872489ced30bba12
Valerie Bolden <valerie.bolden@gmail.com>
2026-05-06 10:25:28 -0400

    added SearchInput and RegionFilter components to UI render

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,8 +1,16 @@
 import CountryCard from "../country/CountryCard";
+import SearchInput from "../filter/SearchInput";
+import RegionFilter from "../filter/RegionFilter";
 
 function HomePage() {
     return (
-        <CountryCard />
+        <div>
+            <div className="filter-search">
+                <SearchInput />
+                <RegionFilter />
+            </div>
+            <CountryCard />
+        </div>
     )
 }
 
