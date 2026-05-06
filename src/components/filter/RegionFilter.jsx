4cdf35ffb2c2007415e7b0f2c1810159e73d1aee
Valerie Bolden <valerie.bolden@gmail.com>
2026-05-06 08:38:30 -0400

    added useState for filter values

--- src/components/filter/RegionFilter.jsx
+++ src/components/filter/RegionFilter.jsx
@@ -1,7 +1,8 @@
 import Select from "react-dropdown-select";
+import { useState } from "react";
 
 function RegionFilter() {
-
+    const [value, setValue] = useState([]);
 }
 
 export default RegionFilter;
