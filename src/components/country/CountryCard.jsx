f177b4e73a2d324bba3aa1de00460ff1056f3c0f
Valerie Bolden <valerie.bolden@gmail.com>
2026-05-07 09:53:26 -0400

    updated card to accept country prop

--- src/components/country/CountryCard.jsx
+++ src/components/country/CountryCard.jsx
@@ -1,12 +1,12 @@
-function CountryCard() {
+function CountryCard({country}) {
     return (
         <div className="card">
-            <img className="flag" src="https://flagcdn.com/pe.svg" alt="" />
+            <img className="flag" src={country.flags.png} alt={country.name.common} />
             <div className="card-body">
-                <h3>Peru</h3>
-                <p>Population: </p>
-                <p>Region: </p>
-                <p>Capital: </p>
+                <h3>{country.name.common}</h3>
+                <p>Population: {country.population}</p>
+                <p>Region: {country.region}</p>
+                <p>Capital: {country.capital?.[0]}</p>
             </div>
         </div>
     );
