39620b0ba13dfa4a232e3e0400c372edd653ddc0
Lateyko Adderley <58910934+uries242@users.noreply.github.com>
2026-05-08 14:41:26 +0000

    Removed redundant and commented code

--- src/App.jsx
+++ src/App.jsx
@@ -26,26 +26,3 @@ function App() {
 }
 
 export default App
-
-// import { useState } from 'react'
-// import { CountryProvider } from './backend/context/CountryContext.jsx'
-// import Trivia from './components/country/Trivia'
-// import Header from './components/filter/Header'
-// import HomePage from './components/pages/Home'
-// import './App.css'
-
-
-// function App() {
-//   const [triviaOpen, setTriviaOpen] = useState(false)
-
-//   return (
-//     <CountryProvider>
-//       <Header />
-//       <button onClick={() => setTriviaOpen(true)}>Play Trivia</button>
-//       <HomePage />
-//       <Trivia isOpen={triviaOpen} onClose={() => setTriviaOpen(false)} />
-//     </CountryProvider>
-//   )
-// }
-
-// export default App
--- src/components/filter/Filters-s.jsx
+++
@@ -1,9 +0,0 @@
-// import RegionFilter from "./RegionFilter";
-
-// export default function Filters({ region, setRegion }) {
-//   return (
-//     <div className="filters">
-//       <RegionFilter region={region} setRegion={setRegion} />
-//     </div>
-//   );
-// }
